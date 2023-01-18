ca8361cce2836dbff832adedd2f25f1e366d790b
yahdielo <yahdielaz@gmail.com>
2023-01-18 03:51:37 +0000

    print 3 times a string

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("Holberton SchoolHolberton School Holberton School")
+print("Holberton SchoolHolberton SchoolHolberton School")
 print("Holberton")
