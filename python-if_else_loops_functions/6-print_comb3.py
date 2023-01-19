9ad2f9c739c2509541b1ded006acc8394d7ff1e4
yahdielo <yahdielaz@gmail.com>
2023-01-19 20:17:53 +0000

    print combinations of number

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(1, 10):
+
+        if i > j:
+            continue
+
+        if j != i and i != j:
+
+            if i == 8 and j == 9:
+                print("{}{}".format(i, j))
+
+            else:
+                print("{}{}, ".format(i, j), end="")
