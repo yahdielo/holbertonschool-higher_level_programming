e6c4104659541db8d0abd3b976b2983f7f77dec8
yahdielo <yahdielaz@gmail.com>
2023-01-19 21:05:23 +0000

    change to upper case

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+
+    for i in str:
+        character = ord(i)
+        
+        if character >= 97 and character <= 122:
+            character = character - 32;
+
+            char = chr(character)
+
+            print("{}".format(char), end="")
+
+    print("")
