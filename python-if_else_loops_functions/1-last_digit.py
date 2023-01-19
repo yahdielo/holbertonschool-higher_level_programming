bf02f590946a5f5c8ce7891369aa37f5ad76c605
yahdielo <yahdielaz@gmail.com>
2023-01-19 15:32:11 +0000

    check if last digit is positive or negativex

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,17 +6,21 @@ num_str = repr(number)
 
 last_digit = num_str[-1]
 
+less_zero = "and is less than 6 and not 0"
+
+bigger_five = "and is greater than 5"
+
 if number < 0:
     sign = "-"
-    print(f"Last digit of {number} is {sign}{last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {sign}{last_digit} {less_zero}")
 else:
 
     if last_digit == "0":
         print(f"Last digit of {number} is 0 and is {last_digit}")
 
     elif last_digit > "5":
-        print(f"Last digit of {number} is {last_digit} and is greater than 5")
+        print(f"Last digit of {number} is {last_digit} {bigger_five}")
 
     elif last_digit < "6" and last_digit > "0":
-        print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last_digit} {less_zero}")
 
