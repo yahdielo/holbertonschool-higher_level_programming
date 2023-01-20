4c90641c25f2af7c248aca6315128d95431846fa
yahdielo <yahdielaz@gmail.com>
2023-01-20 16:25:14 +0000

    exponent

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -3,15 +3,4 @@
 
 def pow(a, b):
 
-    for i in "b":
-
-        if a < 0:
-            power = a * a
-        elif b == 0:
-            power = 1
-        elif b < 0:
-            power = 1 / (a * a)
-        else:
-            power = a * a
-
-    return power
+    return (a ** b)
