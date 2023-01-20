e8ed6c006acd8c6cc72bdb1939a702eda0e3101e
yahdielo <yahdielaz@gmail.com>
2023-01-20 23:07:28 +0000

    calculator

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__"
+    from calculator_1 impoert add, sub, mul, div
+
+a = 10
+b = 5
+
+plus = "+"
+minus = "-"
+multi = "*"
+divid = "/"
+print("{} {} {} = {}".format(a, plus, b, add(a, b)))
+print("{} {} {} = {}".format(a, minus, b, sub(a, b)))
+print("{} {} {} = {}".format(a, multi, b, mul(a, b)))
+print("{} {} {} = {}".format(a, divid, b, div(a, b)))
