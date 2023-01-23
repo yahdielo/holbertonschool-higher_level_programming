3a843a7cdd0c9f97f80a7e5fecfd414e1b098dcc
yahdielo <yahdielaz@gmail.com>
2023-01-23 16:06:22 +0000

    get int from specific index in the list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+
 def element_at(my_list, idx):
 
     if idx < 0:
         return None
 
-    lenght = len(my_list)
+    lenght = len(my_list) - 1
 
     if idx > lenght:
         return None
 
-    print("{:d}".format(my_list[idx]))
+    return my_list[idx]
