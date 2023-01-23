ec9959815b03f2e991d09b68a89ac903ac4ea9fe
yahdielo <yahdielaz@gmail.com>
2023-01-23 17:36:10 +0000

    replace element in the list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -11,7 +11,7 @@ def replace_in_list(my_list, idx, element):
     if idx > lenght:
         return my_list
 
-    my_list.pop([idx])
-    my_list.insert(idx, element)
+    my_list[idx] = element 
+ #   my_list.insert(idx, element)
 
     return my_list
