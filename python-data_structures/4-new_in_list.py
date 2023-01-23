dafe730f8f7615619716b4e2fd59de508714ab3d
yahdielo <yahdielaz@gmail.com>
2023-01-23 18:15:52 +0000

    compy list and chanche value at index

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,15 +4,11 @@
 def new_in_list(my_list, idx, element):
 
     new_list = my_list
-
-    if idx < 0:
-        return new_list
-
     lenght = len(my_list) - 1
 
-    if idx > lenght:
+    if idx > lenght or idx < 0:
         return new_list
 
     new_list[idx] = element
 
-    return new_list, my_list
+    return new_list
