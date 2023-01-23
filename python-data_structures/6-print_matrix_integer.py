82f7f2fdd60795a0e082566c7a770af5d3042578
yahdielo <yahdielaz@gmail.com>
2023-01-23 21:31:27 +0000

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,9 +4,11 @@ def print_matrix_integer(matrix=[[]]):
 
     lenght = len(matrix)
     for i in matrix:
+        count = 0
         for j in i:
+            count += 1
             if j < lenght:
                 print("{:d}".format(j), end=" ")
-            else:
+            elif count < lenght:
                 print("{:d}".format(j), end="")
         print("")
