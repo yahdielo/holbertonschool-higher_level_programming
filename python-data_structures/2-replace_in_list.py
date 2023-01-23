ac33111ecdcd065c7eae80100d65359af382da5b
yahdielo <yahdielaz@gmail.com>
2023-01-23 17:28:49 +0000

    replace element in the list

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhton3
+
+
+def replace_in_list(my_list, idx, element):
+
+    if idx < 0:
+        return mylisti
+
+    lenght = len(my_list) - 1
+
+    if idx > lenght:
+        return my_list
+
+    my_list.insert(idx, element)
+
+    return my_list
