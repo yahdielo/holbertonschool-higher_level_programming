7a22e4e58cd27de9aaea705ec514a3afd422bba9
yahdielo <yahdielaz@gmail.com>
2023-01-23 06:26:28 +0000

    return index od a list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+
+    if idx < 0:
+        return None:
+
+    length = len(my_list)
+
+    if idx > lenght:
+        return None:
+
+    print("{}".format(my_list(idx)))
