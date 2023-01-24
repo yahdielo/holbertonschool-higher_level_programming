59241be1c75180004072b46bb1fe8fb6d8e3875f
yahdielo <yahdielaz@gmail.com>
2023-01-24 19:00:54 +0000

    find divisible

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -5,13 +5,9 @@ def divisible_by_2(my_list=[]):
 
     lenght = len(my_list)
     new_list = list()
-
-    for i in "lenght":
-
+    for i in range(0, lenght):
         if i % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
     return new_list
-
-
