a1722edff518f24fa0862e2e212b0ff212c87fe9
yahdielo <yahdielaz@gmail.com>
2023-01-24 17:03:58 +0000

    add tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,11 +7,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     lenght_a = len(tuple_a)
     lenght_b = len(tuple_b)
+    list_tuple = 0
     #diference_of_lenght = 0
     for i in range(lenght_a):
         if lenght_a > lenght_b:
             lenght_a = lenght_a - 1
-            tuple_a = list.pop([lenght_a])
+            list_tuple = list(tuple_a)
+            tuple_a = list_tuple.pop([lenght_a])
+            new_tuple = tuple(tuple_a)
+            #new_tuple = (list_tuple[0], list_tuple[1])
             res.append(tuple_a[i] + tuple_b[i])
         else:
             res.append(tuple_a[i] + tuple_b[i])
