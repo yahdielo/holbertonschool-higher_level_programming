81809372bce2ffc7c064e1fa942a49c8847dd8bc
yahdielo <yahdielaz@gmail.com>
2023-01-24 18:37:56 +0000

    return biggest number in list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def max_integer(my_list=[]):
 
-    if my_list == None or len(my_list) == 0:
+    if my_list is None or len(my_list) == 0:
         return None
 
     my_list.sort()
