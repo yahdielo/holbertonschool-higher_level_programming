fd88d28ad493971aa55e1a14956caf4b4b4ca760
yahdielo <yahdielaz@gmail.com>
2023-01-24 14:51:02 +0000

    add tuples

---
+++ python-data_structures/7-add_tuplbe.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    res = []
+
+    for i in range(len(tuple_a)):
+        res.append(tuple_a[i] + tuple_b[i])
+    res = tuple(res)
+
+    return res
