97f4b441a70f3aee94b007cb62aeb24743ebb2d1
yahdielo <yahdielaz@gmail.com>
2023-01-24 18:12:55 +0000

    multiple returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -5,13 +5,13 @@ def multiple_returns(sentence):
     count = 0
 
     if sentence is None:
+        sentence[0] = None 
         _tuple = (count, None)
         return _tuple
-
     else:
         for i in  sentence:
             count += 1
 
-        _tuple = (count, sentence[0])
+    _tuple = (count, sentence[0])
 
     return _tuple
