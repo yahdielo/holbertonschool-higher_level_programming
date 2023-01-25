f0ea8691d29344a901bb31fcd5c0d851fe336a54
yahdielo <yahdielaz@gmail.com>
2023-01-25 14:58:07 -0400

    replace all acurrences in the list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -6,7 +6,6 @@ def search_replace(my_list, search, replace):
     _list = list(my_list)
 
     for i in range(len(my_list)):
-        print(i)
         if my_list[i] is search:
             _list[i] = replace
     return _list
