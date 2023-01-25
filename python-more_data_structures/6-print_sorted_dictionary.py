3729bd87c37fece59fa7dedd27efb9591943e46b
yahdielo <yahdielaz@gmail.com>
2023-01-25 17:52:36 -0400

    sort keys in dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,7 +3,7 @@
 
 def print_sorted_dictionary(a_dictionary):
 
-  sorted_keys = list(a_dictionary)
-  sorted_keys.sort()
-  for i in sorted_keys:
-    print("{}: {}".format(i, a_dictionary.get(i)))
+    sorted_keys = list(a_dictionary)
+    sorted_keys.sort()
+    for i in sorted_keys:
+        print("{}: {}".format(i, a_dictionary.get(i)))
