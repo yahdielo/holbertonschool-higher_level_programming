c5d57c1c1a397422510226501f4b1756c5b2b47c
yahdielo <yahdielaz@gmail.com>
2023-01-26 21:44:22 -0400

    roman numbers

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,13 +5,13 @@ def roman_to_int(roman_string):
 
     roman_numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
+    if isinstance(roman_string, str) is False or roman_string is None:
+        return 0
+
     number_sum = 0
     reverse_numbers = reversed(roman_string)
     reverse_list = []
     _numberold = 0
-
-    if roman_string is None:
-        return 0
 
     if len(roman_string) == 1:
         number_sum = roman_numbers.get(roman_string)
