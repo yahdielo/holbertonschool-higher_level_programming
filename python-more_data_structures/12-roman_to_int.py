8239782260c010afa4497a205616e20c130495db
yahdielo <yahdielaz@gmail.com>
2023-01-26 21:46:24 -0400

    roman numbers

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,13 @@
 
 def roman_to_int(roman_string):
 
-    roman_numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_numbers = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500, "M": 1000}
 
     if isinstance(roman_string, str) is False or roman_string is None:
         return 0
