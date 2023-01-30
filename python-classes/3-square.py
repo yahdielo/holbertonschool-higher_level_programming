5ef990234970b5ff4342d548d636f40544262c6f
yahdielo <yahdielaz@gmail.com>
2023-01-30 14:15:08 -0400

    return area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""tThis module write a class Square that defines a square by: (based on 2-square.py)"""
+"""this module write a class Square that defines a square by: (based on 2-square.py)"""
 
 
 class Square:
