1dfd9d791be180b6566387b5d91f980c01ee9a95
yahdielo <yahdielaz@gmail.com>
2023-01-30 14:36:45 -0400

    return area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,19 +3,12 @@
 
 
 class Square:
-   """Defines a square of a particular size
-    Attributes:
-        size (int): size of the square (private)
-    Methods:
-    __init__: initialize specific square
-    size attribute instance"""
+    """This module provides a Class that defines a square"""
     def __init__(self, size=0):
-         if type(size) != int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        
+        self.__size = size
     def area(self):
-        return (self.__size * self.__size)
+        return self.__size * self.__size
