9db1501e8ce73394de499efac6a2c90d06ffa7b0
yahdielo <yahdielaz@gmail.com>
2023-01-30 13:47:51 -0400

    size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """Write a class Square that defines a square by: (based on 1-square.py)"""
     def __init_(self, size=0):
         if type(size) != int:
-            TypeError: size must be a integer
+            TypeError: (("size must be a integer")
         elif size < 0:
-            ValueError: size must be >= 0
+            ValueError: ("size must be >= 0")
         else:
             self.__size = size
