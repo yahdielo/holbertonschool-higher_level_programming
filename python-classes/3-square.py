c9105eff80add0900781c242841bfc2cc971c9cc
yahdielo <yahdielaz@gmail.com>
2023-01-30 14:16:26 -0400

    return area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""this module write a class Square that defines a square by: (based on 2-square.py)"""
+"""this module write a class Square that defines 
+a square by: (based on 2-square.py)"""
 
 
 class Square:
-    """class square has to be an int area(self) returns the current square area"""
+    """class square has to be an int area(self) 
+    returns the current square area"""
     def __init__(self, size=0):
          """class square has to be an int area(self) returns the current square area"""
          if type(size) != int:
