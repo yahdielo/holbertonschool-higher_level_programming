91643bd83e83cebd8e8e4bcbb8a4f99248bb657d
yahdielo <yahdielaz@gmail.com>
2023-01-30 13:15:53 -0400

    private size atribute in class

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Write an empty class Square that defines a square"""
+
+
+class Square:
+    """ Write an empty class Square that defines a square"""
+    def __init__(self, size):
+        self.__size = size
