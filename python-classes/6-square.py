5989758fd533f30d46c4692f3f3fe544caf7876d
yahdielo <yahdielaz@gmail.com>
2023-01-31 17:10:43 -0400

    last one

---
+++ python-classes/6-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""private instace atribute"""
+
+
+class Square:
+    """private instance atribute"""
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        if type(position) != tuple or len(position) != 2:raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[0]) != int or type(position[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+      
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            if self.__position[1] > 0:
+                for idy_position in range(self.__position[1]):
+                    print("")
+            for i in range(self.__size):
+                if self.__position[0] > 0:
+                    for idx in range(self.__position[0]):
+                        print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
