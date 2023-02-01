b4fc62a4304a1506333d12a4bd2dcff568cbba0b
yahdielo <yahdielaz@gmail.com>
2023-02-01 13:25:32 -0400

    first commit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This module supplys add_integer function,
+if value if float, it will be cast to integer and added,
+if value are not int, type error will execute"""
+
+
+def add_integer(a=10, b=98):
+    """Fucntion that adds two integers
+    a (int): fisrt value
+    b (int): second value"""
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+    elif type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    return a + b
