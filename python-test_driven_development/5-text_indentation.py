b9d34fbc8327c6b81b69b189d285d5c3a31c5900
yahdielo <yahdielaz@gmail.com>
2023-02-02 16:58:08 -0400

    text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""This module iterates in a string and finds special characters
+prints them and creates two new lines"""
 
 
 def text_indentation(text):
+    """This fucntion iterates in a string if specified characters
+    are found they will get printed and then two new lines
+    text (str): text must be type str"""
 
     if type(text) != str:
         raise TypeError("text must be a string")
