69b0b4f5698794b422443f13013801041c40bb35
yahdielo <yahdielaz@gmail.com>
2023-02-03 15:01:37 -0400

    reactangle class

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,11 +8,11 @@ class Rectangle:
         if type(width) != int:
             raise TypeError("width must be an integer")
         elif width < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         if type(height) != int:
             raise TypeError("height must be a integer")
         elif height < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         self.__height = height
         self.__width = width
 
@@ -28,7 +28,7 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @width.setter
@@ -36,5 +36,5 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         self.__width = value
