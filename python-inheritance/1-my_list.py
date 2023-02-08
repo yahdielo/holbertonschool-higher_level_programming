fb189e985b08281fdfd511ca72da0b18a91d8689
Yahdiel Saldana Rios <yahdielsaldanarios@Yahdiels-MBP.localdomain>
2023-02-08 15:05:28 -0400

    inherits

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
-this module  inherits a list and then sorts the sorts the content of the list
+this module  inherits a list 
+and then sorts the sorts the 
+content of the list
 """
 
 
 class Mylist(list):
     """
-    this class Mylist inherits a list and then creat a new one with the numbers sorted
+    this class Mylist inherits a list 
+    and then creat a new one with 
+    the numbers sorted
     """
     def print_sorted(self):
-        """this function sort a list"""
+        """this function 
+        sort a list"""
         new_list = list(list)
         for i in new_list:
             self.new_list.sort()
