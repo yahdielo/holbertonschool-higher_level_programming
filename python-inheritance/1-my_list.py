2a72284df797a835c67a0cad8771d13575e811c4
yahdielo <yahdielaz@gmail.com>
2023-02-08 16:03:13 -0400

    fix doc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,8 @@
 class MyList(list):
     """Defines a new class MyList that inherits from list"""
     def print_sorted(self):
-        new_list = list(list)
-        self.new_list.sort()
-        return new_list
+        new_list = []
+        for i in list:
+            new_list.append(i)
+        return new_list.sort()
+
