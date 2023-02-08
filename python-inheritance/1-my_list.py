c309a04f746d972d8e86ccf695ed3fb3ed1f6b4b
yahdiel0 <38517971+yahdielo@users.noreply.github.com>
2023-02-08 15:40:34 -0400

    fix documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,7 +7,7 @@ content of the list
 
 
 class Mylist(list):
-    """Class that inherits list as a parent and sort the list and retuns it"""
+    """Class that inherits list as a parent, creat a new list a sorts it"""
     def print_sorted(self):
         new_list = list(list)
         self.new_list.sort()
