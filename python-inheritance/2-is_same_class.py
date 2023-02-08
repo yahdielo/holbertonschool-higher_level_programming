a07189aee92a754d4bc0bfa7de5598f07fc203cd
yahdielo <yahdielaz@gmail.com>
2023-02-08 16:35:50 -0400

    function to check obj and class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module provide a new class called MyList"""
+
+
+def is_same_class(obj, a_class):
+    """This module provide a new class called MyList"""
+    if obj == a_class:
+        return True
+    else:
+        return False
