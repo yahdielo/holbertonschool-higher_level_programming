77f3db11deccbdc57928e908f1cbd437f34a1c63
Yahdiel Saldana Rios <yahdielsaldanarios@Yahdiels-MBP.localdomain>
2023-02-08 13:20:09 -0400

    print all obj in class

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module retuns all objects in the class method"""
+
+
+def lookup(obj):
+    """Return all bojects inside the class that pass as parameter"""
+
+    return(dir(obj))
