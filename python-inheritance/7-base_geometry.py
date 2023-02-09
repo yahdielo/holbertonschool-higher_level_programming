eee100169d2b9e3bceef69ea94a6c131ff167aa7
yahdielo <yahdielaz@gmail.com>
2023-02-09 13:20:53 -0400

    raise exception

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""this fucntions checks if objec is a instance of the parent class"""
+
+
+class BaseGeometry:
+    """this fucntions checks if objec is a instance of the parent class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise TypeError("<name> must be greater than 0")
