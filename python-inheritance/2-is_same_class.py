f0cf437d912a8940406a50c631ad245f774fffb6
yahdielo <yahdielaz@gmail.com>
2023-02-09 11:04:29 -0400

    is class?

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,9 +4,4 @@
 
 def is_same_class(obj, a_class):
     """This module provide a new class called MyList"""
-    if obj == a_class:
-        return True
-    if type(obj) == type(a_class):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
