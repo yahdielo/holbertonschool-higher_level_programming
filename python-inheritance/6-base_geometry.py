e96a9b3fdd93c912d5b53efc32d3820f53460834
yahdielo <yahdielaz@gmail.com>
2023-02-09 13:11:58 -0400

    raise exception base geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this fucntions checks if objec is a instance of the parent class"""
+
+
+class BaseGeometry:
+    """this fucntions checks if objec is a instance of the parent class"""
+    def area(self):
+        raise Exception("area() is not implemented")
