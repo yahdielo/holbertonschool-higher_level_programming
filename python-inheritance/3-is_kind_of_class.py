82b0ae7f9c390c3f86926fc18f02949372c5ed0d
yahdielo <yahdielaz@gmail.com>
2023-02-09 11:07:58 -0400

    is instance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/usr/python3
+"""this fucntions checks if objec is a instance of the parent class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """this fucntions checks if objec is a instance of the parent class"""
+    return isinstance(obj, a_class)
