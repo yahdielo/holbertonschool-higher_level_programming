e799c093bb61d1956cbbd34b6c9071dc9eadd36e
yahdielo <yahdielaz@gmail.com>
2023-02-09 13:05:43 -0400

    base geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this fucntions checks if objec is a instance of the parent class"""
+
+
+class BaseGeometry:
+    """this fucntions checks if objec is a instance of the parent class"""
+    pass
