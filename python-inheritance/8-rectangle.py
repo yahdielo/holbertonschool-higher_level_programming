06971d87434402ec9689db31685e84671b7717f8
yahdielo <yahdielaz@gmail.com>
2023-02-09 15:12:35 -0400

    inherit integer validator

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -10,5 +10,5 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.__height = height 
+        self.__height = height
         self.__width = width
