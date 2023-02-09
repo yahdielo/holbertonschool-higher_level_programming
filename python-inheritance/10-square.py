d9612973f1adf2e74845f2e2cc31cd053402ca32
yahdielo <yahdielaz@gmail.com>
2023-02-09 15:55:33 -0400

    inherit integer validator

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,17 +2,11 @@
 """command"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square:
+class Square(Rectangle):
     """command"""
     def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__size = size
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return (f"[Rectangle] {self.__width}/{self.__height}")
+        super().integer_validator("size", size)
+        super().__init__(size, size)
