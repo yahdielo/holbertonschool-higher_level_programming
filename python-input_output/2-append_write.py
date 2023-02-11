aab5f847aef2264af62c3f05008bb9178eca6bbf
yahdielo <yahdielaz@gmail.com>
2023-02-11 18:36:02 -0400

    append at the end

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""comment"""
 
 
 def append_write(filename="", text=""):
+    """comment"""
     with open(filename, "a", encoding="utf-8") as f:
         f.write(text)
         return  len(text)
