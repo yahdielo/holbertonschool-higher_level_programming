c25d5a5c0baf17798e8ecd62cfba830c4c614de0
yahdielo <yahdielaz@gmail.com>
2023-02-11 19:10:29 -0400

    write json format to file

--- python-input_output/5-save_to_json_file.json.py
+++ python-input_output/5-save_to_json_file.json.py
@@ -3,6 +3,7 @@
 import json
 
 def save_to_json_file(my_obj, filename):
+    """comment"""
     with open(filename, "w", encoding="utf-8") as f:
         myjson_string = json.dumps(my_obj)
         return f.write(myjson_string)
