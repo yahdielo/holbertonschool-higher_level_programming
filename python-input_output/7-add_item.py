528de9d2c2dfd49c108daed1b566bb67df577b25
yahdielo <yahdielaz@gmail.com>
2023-02-11 20:40:47 -0400

    append arguments to a list of list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,11 +9,12 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arguments = sys.argv
 filename = 'add_items.json'
+my_list = []
 
 try:
     laod_obj = load_from_json_file(filename)
 except:
-    my_list = []
+    pass
 for i in arguments[1:]:
     my_list.append(i)
 save_to_json_file(my_list, filename)
