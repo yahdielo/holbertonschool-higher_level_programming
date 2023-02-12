faef7f8cf4f84bafe6c98db38c0f4f5ddd8bfcc9
yahdielo <yahdielaz@gmail.com>
2023-02-11 20:15:49 -0400

    append arguments to a list of list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-THis module reads argumends and appends them to a list
-in json format
+THis module reads argumends and appends them to a list in json format
 """
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
