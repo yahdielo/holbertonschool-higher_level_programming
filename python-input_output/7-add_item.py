7fd91e5d07fc2f33ed618e61072b9ce2cecf5328
yahdielo <yahdielaz@gmail.com>
2023-02-11 20:03:26 -0400

    append arguments to a list of list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,6 +12,7 @@ filename = 'add_items.json'
 
 with open(filename, "a+", encoding="utf-8") as f:
     my_list = []
-    my_list.extend(arguments[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+    for i in len(arguments):
+        my_list.append(arguments[i])
+        save_to_json_file(my_list, filename)
+        load_from_json_file(filename)
