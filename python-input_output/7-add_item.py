38c805a5426528cfbd8c6f1bc434ab51904e93ec
yahdielo <yahdielaz@gmail.com>
2023-02-11 20:50:37 -0400

    append arguments to a list of list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-Script: Adds all arguments to a Python list,
-and then saves them to a file
-"""
+"""comment"""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
-filename = "add_item.json"
+arguments = sys.argv
+filename = 'add_items.json'
 my_list = []
 
 try:
     my_list = load_from_json_file(filename)
 except:
     pass
-for i in range(1, len(args)):
-    my_list.append(args[i])
+for i in range(1, len(arguments)):
+    my_list.append(arguments[i])
 save_to_json_file(my_list, filename)
