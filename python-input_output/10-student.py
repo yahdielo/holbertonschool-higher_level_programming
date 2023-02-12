46451ca463359a30bef4292002d2ea21734fed74
yahdielo <yahdielaz@gmail.com>
2023-02-11 21:43:26 -0400

    object dicitonery

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""comment"""
+"""
+Class Student
+"""
 
 
 class Student:
-    """comment"""
-
+    """
+    Defines a student
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        obj_dict = {}
-        if attrs is None:
-            attrs = dir(self)
-        for i in attrs:
-            if i in dir(self):
-                obj_dict[i] = getattr(self, i)
-            return obj_dict
+        if attrs is not None:
+            my_dict = {}
+            for i in attrs:
+                if str(i) == "first_name":
+                    my_dict[i] = self.first_name
+                elif i == "last_name":
+                    my_dict[i] = self.last_name
+                elif i == "age":
+                    my_dict[i] = self.age
+            return my_dict
+        else:
+            return self.__dict__
