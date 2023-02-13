8b62c1e500f1a4ab11f0166a9e3b24423fc85ee7
yahdielo <yahdielaz@gmail.com>
2023-02-13 16:05:35 -0400

    rectangle edit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -41,7 +41,7 @@ class Rectangle(Base):
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         else:
             self.__width = value
