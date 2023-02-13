787bfaf8e5921140e15436848403148c16ecf498
yahdielo <yahdielaz@gmail.com>
2023-02-13 16:04:31 -0400

    rectangle edit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -67,7 +67,7 @@ class Rectangle(Base):
     def x(self, value):
         if type(value) != int:
             raise TypeError("x must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
         else:
             self.__x = value
@@ -80,7 +80,7 @@ class Rectangle(Base):
     def y(self, value):
         if type(value) != int:
             raise TypeError("y must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
