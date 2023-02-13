0276a042a051a04e4107d001f2aa57563df5be48
yahdielo <yahdielaz@gmail.com>
2023-02-13 16:16:22 -0400

    add area to rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -34,6 +34,7 @@ class Rectangle(Base):
         self.__y = y
 
     def area(self):
+        """this module creturns the area of a rectangle"""
         return self.__width * self.__height
 
     @property
