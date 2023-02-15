59b22fe45358c4bb47657e3e590c31d7d7dc1433
yahdielo <yahdielaz@gmail.com>
2023-02-15 16:04:39 -0400

    return Square methods as dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,6 +22,7 @@ class Base:
             self.id = id
 
     def to_json_string(list_dictionaries):
+        """comment"""
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
