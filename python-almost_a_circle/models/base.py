55cf9f1d19c9a96290d32ddb89328a4f8a8a0a3e
yahdielo <yahdielaz@gmail.com>
2023-02-15 16:24:19 -0400

    return dictionary in to a file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -28,7 +28,7 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
-    @staticmethod
+    @classmethod
     def save_to_file(cls, list_objs):
         """comment"""
         filename = f"{cls.__name__}.json"
