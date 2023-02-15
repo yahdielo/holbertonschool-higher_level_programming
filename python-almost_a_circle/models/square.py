7b600990ddc0e910fec36a05c27f28eef63cedff
yahdielo <yahdielaz@gmail.com>
2023-02-15 15:04:05 -0400

    added getter and setter for height and width

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-    
+
     @property
     def size(self):
         return super().width
