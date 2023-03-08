1a3f67588d3a67412475f9b11617c92c5a7cbae7
yahdielo <yahdielaz@gmail.com>
2023-03-08 16:46:44 -0400

    creat new table and  multiple rowes

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- create a new table and add new rows
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (`id` int(10), `name` varchar(256), `score` int(10));
+CREATE TABLE IF NOT EXISTS second_table (`id` int(10), `name` varchar(256), `score` int(10));
 INSERT INTO second_table (`id`, `name`, `score`) Values ('1', 'John', '10'), ('2', 'Alex', '3'), ('3', 'Bob', '14'), ('4', 'George', '8'); 
