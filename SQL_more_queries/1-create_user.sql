0e9d9ae123dcfd19c02b68b08915ec33c44190e6
yahdielo <yahdielaz@gmail.com>
2023-03-11 23:33:53 -0400

    create new user with all grants

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create a new user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO USER 'user_0d_1'@'localhost';
