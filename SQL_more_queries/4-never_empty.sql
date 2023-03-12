60b852c864a4d7c2e46614409ed1cf67d8a1f6f1
yahdielo <yahdielaz@gmail.com>
2023-03-12 14:51:41 -0400

    creat table with default value

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,5 @@
 -- creat table with default value
-CREATE TABLE IF NOT EXISTS id _not_null (id INT DEFAULT 1, name varchar(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS id _not_null (
+    id INT DEFAULT 1, 
+    name varchar(256) NOT NULL
+);
