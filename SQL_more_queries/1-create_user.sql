7cc94efc6e53cb50cd157fece0ca52e1ff561504
yahdielo <yahdielaz@gmail.com>
2023-03-11 23:29:56 -0400

    create new user with all grants

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- create a new user
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIDIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
