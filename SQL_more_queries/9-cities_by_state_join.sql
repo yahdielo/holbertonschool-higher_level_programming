fc4ccd2b368027beb93fd40f9a474e825f3e4eb4
yahdielo <yahdielaz@gmail.com>
2023-03-12 16:23:22 -0400

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,5 @@
 -- list all cities in databse
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN state ON cities.state_id = states.id;
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+JOIN states ON cities.state_id = states.id
+OEDER BY cities.id ASC;
