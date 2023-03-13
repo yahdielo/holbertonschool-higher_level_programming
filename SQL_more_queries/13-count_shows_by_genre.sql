790c6a20bc10c25ced1f858ed806d3cc578a47f1
yahdielo <yahdielaz@gmail.com>
2023-03-12 23:03:10 -0400

    task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- shows by genre
+SELECT a.name AS genre, COUNT(b.genre_id) AS number_of_shows
+FROM tv_genres a
+JOIN tv_show_genres b
+ON a.id = b.genre_id
+GROUP BY b.genre_id
+ORDER BY number_of_shows DESC;
