485ada304020521e830927297ab8d7d6791c397b
yahdielo <yahdielaz@gmail.com>
2023-03-14 11:35:35 -0400

    query states id from database asc order

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+
+import MySQLdb
+
+
+def start_letter(argv):
+    """ This module quierys a nae starting with specific letter"""
+
+    conn = MySQLdb.connect(host="localhost",
+                                port=3306, user=sys.argv[1],
+                                passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = conn.cursor()
+
+    # sql command to execute
+    cur.execute("select * from  hbtn_0e_0_usa.states where name like 'N%' ORDER BY states.id ASC")
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    start_letter(sys)
