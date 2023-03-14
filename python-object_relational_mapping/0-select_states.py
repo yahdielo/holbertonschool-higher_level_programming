4d881ff6af0b316d25f0204f068713ebcfcb9627
yahdielo <yahdielaz@gmail.com>
2023-03-13 23:31:00 -0400

    tasl 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,7 @@ import MySQLdb
 
 
 def states(argv):
-
-    """ Documented module , is documented """
+    """ this module is documented """
 
     conn = MySQLdb.connect(host = "localhost", port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     cur = conn.cursor()
@@ -20,6 +19,5 @@ def states(argv):
         print(row)
     cur.close()
     conn.close()
-
 states(sys)
 
