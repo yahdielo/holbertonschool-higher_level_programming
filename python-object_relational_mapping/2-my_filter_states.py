a6cb9a87ecddcaaf855f623f0609483dfdd1c16b
yahdielo <yahdielaz@gmail.com>
2023-03-14 14:15:57 -0400

    query specific value from column

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This module quierys a nae starting with specific letter """
+
+import sys
+
+import MySQLdb
+
+
+def matching_arg(argv):
+    """ This module quierys a name matching argmunet index 4 """
+
+    conn = MySQLdb.connect(host="localhost",
+                                port=3306, user=sys.argv[1],
+                                passwd=sys.argv[2], db=sys.argv[3])
+    arg_name = sys.argv[4]
+
+    cur = conn.cursor()
+
+    # sql command to execute
+    cur.execute(f"SELECT * FROM  states WHERE NAME = '{arg_name}'")
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    matching_arg(sys)
