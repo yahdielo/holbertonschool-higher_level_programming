aebed16edcb22c483bcd77486475b52cda0c630a
yahdielo <yahdielaz@gmail.com>
2023-03-13 23:31:58 -0400

    tasl 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,7 @@ import MySQLdb
 
 
 def states(argv):
-    """ this module is documented """
-
+    '''Function that connect and fetch data'''
     conn = MySQLdb.connect(host = "localhost", port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     cur = conn.cursor()
 
