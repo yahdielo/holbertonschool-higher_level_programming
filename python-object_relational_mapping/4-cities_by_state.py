c2bf9b4f5526d807cd85509b235207d22dd685bb
yahdielo <yahdielaz@gmail.com>
2023-03-14 16:04:38 -0400

    list all cities from data base

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This module quierys a nae starting with specific letter """
+
+import sys
+
+import MySQLdb
+
+
+def matching_arg(argv):
+    """
+    Write a script that lists all cities from the database hbtn_0e_4_usa
+    """
+
+    conn = MySQLdb.connect(host="localhost",
+                                port=3306, user=sys.argv[1],
+                                passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = conn.cursor()
+
+    # sql command to execute
+    str1 = "SELECT * FROM  cities"
+    cur.execute(str1)
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    matching_arg(sys)
