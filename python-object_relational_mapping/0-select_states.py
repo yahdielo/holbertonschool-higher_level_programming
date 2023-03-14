ae06d604b523428d7f26b349bdf368f75c1c03f0
yahdielo <yahdielaz@gmail.com>
2023-03-13 23:12:15 -0400

    tasl 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,10 +9,7 @@ import MySQLdb
 
 
 def states(argv):
-    """ 
-        This module takes 3 arguments from the terminal and connects to mysqldb
-        then lists all states from the data base
-    """
+    """ This module is documented"""
 
     conn = MySQLdb.connect(host = "localhost", port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     cur = conn.cursor()
