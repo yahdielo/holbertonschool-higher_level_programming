e2d3361093f60cd110ed06f28c7a89828c958a5f
yahdielo <yahdielaz@gmail.com>
2023-03-14 17:47:23 -0400

    task 5

---
+++ python-object_relational_mapping/5-filter_citites.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This module quierys a nae starting with specific letter """
+
+import sys
+
+import MySQLdb
+
+
+def matching_arg(argv):
+    """
+    Write a script that lists all cities from the database hbtn_0e_4_usa
+    """
+
+    conn = MySQLdb.connect(host="localhost",
+                                port=3306, user=sys.argv[1],
+                                passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = conn.cursor()
+
+    state = sys.argv[4]
+    # sql command to execute
+    str1 = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE state.id = state_id "
+    cur.execute(str1)
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    matching_arg(sys)
