766df5c41501fbbe412aab0d01350e64a7fe5731
yahdielo <yahdielaz@gmail.com>
2023-03-14 13:57:12 -0400

    query states id from database asc order

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,11 +16,11 @@ def start_letter(argv):
     cur = conn.cursor()
 
     # sql command to execute
-    cur.execute("SELECT * FROM  states WHERE NAME LIKE BINARY 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM  hbtn_0e_0_usa.states WHERE NAME LIKE 'N%' ORDER BY states.id ASC")
 
     query_rows = cur.fetchall()
 
-    for row in query_rows:S
+    for row in query_rows:
         print(row)
     cur.close()
     conn.close()
