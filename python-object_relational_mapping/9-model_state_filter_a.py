382b2df3cf12a786d5e95bdf11739b894f8bf94c
yahdielo <yahdielaz@gmail.com>
2023-03-15 14:24:13 -0400

    task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    str1 = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(str1.format(sys.argv[1],
+                                       sys.argv[2],
+                                       sys.argv[3]), pool_pre_ping=True)
+
+    session = sessionmaker(engine)
+    session = session()
+    states = session.query(State).filter(State.name.like('%a%'))
+    for items in states:
+        if states :
+            print("{}: {}".format(items.id, items.name))
+        else:
+            print("Nothing")
+    session.close()
