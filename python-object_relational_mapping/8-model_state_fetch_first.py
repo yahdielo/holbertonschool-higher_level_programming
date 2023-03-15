415722f45c112f002606eaa35bf33f758ad6920f
yahdielo <yahdielaz@gmail.com>
2023-03-15 13:55:04 -0400

    task 8 filters

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     session = sessionmaker(engine)
     session = session()
-    states = session.query(State).one()
+    states = session.query(State).first()
     if states:
         print("{}: {}".format(states.id, states.name))
     else:
