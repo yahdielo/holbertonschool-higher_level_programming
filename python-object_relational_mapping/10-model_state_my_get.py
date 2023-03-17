2affbe7468990f33bbdd042ab0bd018e0074c0cc
yahdielo <yahdielaz@gmail.com>
2023-03-16 21:22:31 -0400

    added scalar

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
 import sys
+
+from sqlalchemy import (create_engine)
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    db_api = 'mysql+mysqldb://{}:{}@localhost/{}'
-    usr = sys.argv[1]
-    pswd = sys.argv[2]
-    db = sys.argv[3]
-    state = sys.argv[4]
 
-    engine = create_engine(db_api.format(usr, pswd, db), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).filter(State.name.like(state)).scalar()
-    if states is not None:
-        print(states.id)
+    str1 = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(str1.format(sys.argv[1],
+                                       sys.argv[2],
+                                       sys.argv[3]), pool_pre_ping=True)
+    arg_state = sys.argv[4]
+    session = sessionmaker(engine)
+    session = session()
+    states = session.query(State).filter(State.name.like(arg_state)).scalar()
+   
+    if states:
+        print("{}".format(states.id))
     else:
         print("Not found")
+    session.close()
