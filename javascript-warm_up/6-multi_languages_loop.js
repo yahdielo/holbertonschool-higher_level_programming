383e7084f80f8905b461a59c5b7929b15a51395f
yahdielo <yahdielaz@gmail.com>
2023-04-17 15:21:00 -0400

    task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const list_string = ["C is fun", "Python is cool", "JavaScript is amazing"]
-let i = 0
-while ( i < list_string.length) {
-    console.log(list_string[i]);
-    i++;
+const list_string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < list_string.length) {
+  console.log(list_string[i]);
+  i++;
 }
