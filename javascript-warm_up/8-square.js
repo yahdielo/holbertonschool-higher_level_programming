9facba007405eb7ab33e1fc5a710838e7a5b9db2
yahdielo <yahdielaz@gmail.com>
2023-04-17 16:02:41 -0400

    fix logical operaor if statement

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,8 +4,9 @@ const process = require('process');
 const variable = process.argv;
 
 let square = '';
-if (variable.length < 3)
+if (variable.length <= 2 || isNaN(variable[2]) == true)
     console.log('Missing size')
+
 for (let i = 0; i < variable[2]; i++) {
   for (let j = 0; j < variable[2]; j++) {
     square += 'X';
