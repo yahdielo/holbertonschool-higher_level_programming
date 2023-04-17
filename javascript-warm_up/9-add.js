e3e5d0e03bf9f47037b06549c207c5f4cd5f4ab2
yahdielo <yahdielaz@gmail.com>
2023-04-17 16:25:03 -0400

    task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,13 +3,8 @@
 const process = require('process');
 const variable = process.argv;
 
-function add(a, b)
-{
-    if (isNaN(a) === true && isNaN(b) === true)
-        console.log('NaN')
-    else
-        console.log(Number(a) + Number(b))
+function add (a, b) {
+  if (isNaN(a) === true && isNaN(b) === true) { console.log('NaN'); } else { console.log(Number(a) + Number(b)); }
 }
 
-result = add(variable[2], variable[3])
-
+add(variable[2], variable[3]);
