5e1fe9af0070bd25381fc0d0f0738b9e78e62b95
yahdielo <yahdielaz@gmail.com>
2023-04-17 14:42:21 -0400

    task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -11,10 +11,11 @@ if (typeof variable[2] === 'number' &&
     !Number.isNaN(variable[2]) &&
     !Number.isInteger(variable[2])
     ) {
-        console.log('My number: '+ variable[2])
+        console.log('My number: '+ Math.trunc(variable[2]))
     }
     else
-        console.log('My number: ' + variable[2])
+        if (isNaN(variable[2])== false)
+            console.log('My number: ' + variable[2])
         
 if (variable.length == 3)
     if (isNaN(variable[2])== true)
