afec81479a404afc8eee66c00d79b1efc2df25e7
yahdielo <yahdielaz@gmail.com>
2023-04-17 17:18:02 -0400

    return second biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,17 +3,13 @@
 const process = require('process');
 const variable = process.argv;
 
-function second_biggest(list)
-{
-    sorted_list = []
-    if(variable.length <= 3) {
-        return 0;
-    }
-    else
-        sorted_list = variable.sort()
-        return sorted_list[3]
-
+function second_biggest (list) {
+  let sorted_list = [];
+  if (variable.length <= 3) {
+    return 0;
+  } else { sorted_list = variable.sort(); }
+  return sorted_list[sorted_list.length - 2];
 }
-
-result = second_biggest(variable)
-console.log(result)
+let result = 0;
+result = second_biggest(variable);
+console.log(result);
