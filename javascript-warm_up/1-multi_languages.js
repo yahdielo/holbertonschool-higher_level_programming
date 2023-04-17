d78f9963dd662800f102629972179bd625c28956
yahdielo <yahdielaz@gmail.com>
2023-04-17 12:37:23 -0400

    print multiple strings

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const list_string = ["C is fun", "Python is cool", "Javascript is amazing"]
+i = 0
+while ( i < list_string.length) {
+    console.log(list_string[i]);
+    i++;
+}
