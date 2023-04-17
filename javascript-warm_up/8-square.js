9bf7b0e79526221d7882eaa0058cfc0340da8a3e
yahdielo <yahdielaz@gmail.com>
2023-04-17 15:51:18 -0400

    print a square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,9 +4,11 @@ const process = require('process');
 const variable = process.argv;
 
 let square = '';
+if (variable.length < 3)
+    console.log('Missing size')
 for (let i = 0; i < variable[2]; i++) {
   for (let j = 0; j < variable[2]; j++) {
-    square += 'x';
+    square += 'X';
   }
   if (i < variable[2] - 1)
     square += '\n';
