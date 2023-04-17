5cbe3fa0ae3315999d82a34bd1d718a34a21fc57
yahdielo <yahdielaz@gmail.com>
2023-04-17 13:41:57 -0400

    return arguments task 4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -5,5 +5,7 @@ const variable = process.argv
 
 if (variable.length > 3)
     console.log(variable[2] +' is '+ variable[3])
+if (variable.length == 3)
+    console.log(variable[2] +' is undefined')
 else
-    console.log('undefine is undefine')
+    console.log('undefined is undefined')
