d1ee91c63ec2d35b0de2b6ab366aaa5c128d511b
yahdielo <yahdielaz@gmail.com>
2023-04-17 13:28:29 -0400

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,6 +4,6 @@ const process = require('process');
 const variable = process.argv
 
 if (variable.length >= 3)
-    console.log('Arguments found')
+    console.log('Argument found')
 else
     console.log('No arguements')
