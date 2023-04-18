6760a32b980d9c5d77b1955f2dc6afda9fe964c7
yahdielo <yahdielaz@gmail.com>
2023-04-18 16:25:09 -0400

    task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  // print the squre with c and X if c is undefine
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let object = '';
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
+        object += c;
+      }
+      if (i < this.width - 1) { object += '\n'; }
+    }
+    console.log(object);
+  }
+}
+module.exports = Square;
