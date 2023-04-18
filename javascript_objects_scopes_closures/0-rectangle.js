307e284f589f85a64c080c492ddee82db610f6c7
yahdielo <yahdielaz@gmail.com>
2023-04-18 12:50:45 -0400

    declare a class rectangle

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// clas  declaration
+class Rectangle {
+  constructor (height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+module.exports = Rectangle;
