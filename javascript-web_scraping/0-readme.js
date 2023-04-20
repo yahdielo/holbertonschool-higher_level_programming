bea2bb8a2ec6af0a81aa573b8e63e705af840ef2
yahdielo <yahdielaz@gmail.com>
2023-04-20 11:29:00 -0400

    read from file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+var argument = process.argv;
+
+const fs = require('fs')
+fs.readFile(argument[2], (err, inputD) => {
+    if (err) throw err;
+    console.log(inputD.toString());
+})
+
