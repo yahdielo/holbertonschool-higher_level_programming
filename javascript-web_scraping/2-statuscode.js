9bb4b26221948ee653078ab4900dcd6cc0064152
yahdielo <yahdielaz@gmail.com>
2023-04-21 14:16:05 -0400

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const request = require('request')
- 
+const request = require('request');
+
 // Request URL
-let url = process.argv;
+const url = process.argv;
 request(url[2], (error, response, body) => {
-    // Printing the error if occurred
-    if (error) console.log(error)
- 
-    // Printing status code
-    console.log('code:',response.statusCode);
+  // Printing the error if occurred
+  if (error) console.log(error);
+
+  // Printing status code
+  console.log('code:', response.statusCode);
 });
