77ae3aa6fa5c9f98d7e7a584c7d97746fd396ac2
yahdielo <yahdielaz@gmail.com>
2023-04-21 16:19:25 -0400

    find number of movies character appears

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,23 +5,23 @@ const request = require('request');
 // Request URL
 const url = process.argv[2];
 
-request (url, (error, response, body) => {
-    if (error) console.log(error)
+request(url, (error, response, body) => {
+  if (error) console.log(error);
 
-    // convert request in to javascript oobject
-    let object = JSON.parse(body);
-    let count = 0;
+  // convert request in to javascript oobject
+  const object = JSON.parse(body);
+  let count = 0;
 
-    //iterate inside the list contining objects
-    for (item of object.results) {
-        let _list = item.characters;
-    
-        //iterate inside objects
-        for (_string of _list) {
-            if (_string.includes(18)) {
-                count += 1;
-            }
-        }
+  // iterate inside the list contining objects
+  for (item of object.results) {
+    const _list = item.characters;
+
+    // iterate inside objects
+    for (_string of _list) {
+      if (_string.includes(18)) {
+        count += 1;
+      }
     }
-    console.log(count);
+  }
+  console.log(count);
 });
