7e977b15df98123a575ee22ab5c8223a07ba1913
yahdielo <yahdielaz@gmail.com>
2023-04-21 15:20:46 -0400

    getting title of movie pass as arg

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+let id = process.argv[2]
+let url = "https://swapi-api.hbtn.io/api/films/" + id
+
+request(url, (error, response, body ) => {
+    //if i get error , console loge it
+    if (error) console.log(error);
+
+    //converting result from the request in to a javascript object
+    let obj = JSON.parse(body)
+
+    // i can call value of a object by passing the key as mathod to object
+    console.log(obj.title)
+});
