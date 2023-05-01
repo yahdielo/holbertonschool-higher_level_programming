96b8b4d809e83929b34e88d6aecbe11254bbbd8f
yahdielo <yahdielaz@gmail.com>
2023-05-01 17:13:21 -0400

    finish tsdk

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const url = 'https://stefanbohacek.com/hellosalut/?lang=fr'
 $(function() { $.getJSON(url, function(greeting) {
-    $("#hello").append(greeting.hello)
-}) });
+    $('#hello').append(greeting.hello);
+    console.log(greeting.hello);
+}); 
+});
